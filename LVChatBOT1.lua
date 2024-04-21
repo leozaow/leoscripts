95c9bc76f715f8951d2a437667221efca0f51f2f
leozaow <leandrobr9@gmail.com>
2024-04-21 02:13:33 -0300

    Update LVChatBOT1.lua

--- LVChatBOT1.lua
+++ LVChatBOT1.lua
@@ -1,4 +1,4 @@
-local scriptEsperado = "teste" --script
+local scriptEsperado = script
 Game.talkPrivate("[" .. scriptEsperado .. "] Define the language alternative (GPTalternative) in the LVChatBOT.lua file with the options: \n[1] = pt-br \n[2] = english \n[3] = spanish \n[4] = polish", Player.getName())
 
 
