f283ac0ffe4ad77d95ca3a04efb2dbcd2b19c108
leozaow <leandrobr9@gmail.com>
2024-04-21 02:11:11 -0300

    Add files via upload

---
+++ LVChatBOT1.lua
@@ -0,0 +1,6 @@
+local scriptEsperado = "teste" --script
+Game.talkPrivate("[" .. scriptEsperado .. "] Define the language alternative (GPTalternative) in the LVChatBOT.lua file with the options: \n[1] = pt-br \n[2] = english \n[3] = spanish \n[4] = polish", Player.getName())
+
+
+
+
